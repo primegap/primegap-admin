10abb2aa187f82ecb5a45c0f2865f1ae4336a811
Luca Tironi <luca.tironi@gmail.com>
2015-11-09 21:11:11 +0100

    Add admin_users model specs

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'schema' do
+    it { is_expected.to have_db_column(:email).of_type(:string) }
+    it { is_expected.to have_db_column(:encrypted_password).of_type(:string).with_options(default: '', null: false) }
+    it { is_expected.to have_db_column(:reset_password_token).of_type(:string) }
+    it { is_expected.to have_db_column(:reset_password_sent_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:remember_created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:sign_in_count).of_type(:integer).with_options(default: 0, null: false) }
+    it { is_expected.to have_db_column(:current_sign_in_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:last_sign_in_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:current_sign_in_ip).of_type(:string) }
+    it { is_expected.to have_db_column(:last_sign_in_ip).of_type(:string) }
+    it { is_expected.to have_db_column(:created_at).of_type(:datetime) }
+    it { is_expected.to have_db_column(:updated_at).of_type(:datetime) }
+  end
 end
