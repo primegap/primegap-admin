d0fd55c905c5944b90f7dea24e48dbaa1521e011
Luca Tironi <luca.tironi@gmail.com>
2015-11-09 22:15:21 +0100

    Apply rubocop autocorrect

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
     sequence(:full_name) { |n| "first_name_#{n} last_name_#{n}" }
     sequence(:first_name) { |n| "first_name_#{n}" }
     sequence(:last_name) { |n| "last_name_#{n}" }
-    phone "02123456789"
+    phone '02123456789'
     sequence(:email) { |n| "customer#{n}@example.com" }
   end
 end
